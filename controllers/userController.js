d227cc2491c9576a520be39da7692f9ce2d3938f
alexanderOcoro <clasestic2014@gmail.com>
2025-09-08 23:47:47 -0500

    Primer commit backend

---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ mensaje: 'Usuario guardado', user });
+  } catch (err) {
+    res.status(500).json({ error: 'Error al guardar usuario' });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Error al obtener usuarios' });
+  }
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Aquí conectamos la ruta
+app.use('/api/users', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Conectado a MongoDB Atlas');
+    app.listen(process.env.PORT, () => {
+      console.log(`🚀 Servidor en http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error('❌ Error de conexión:', err));
